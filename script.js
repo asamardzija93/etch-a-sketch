987e205157f84d3195810646f0174a61ef4b5ab9
Aleksandar <asamardzija93@gmail.com>
2020-05-11 15:45:36 -0700

    cleaned up code

--- script.js
+++ script.js
@@ -4,107 +4,109 @@ const resetBtn = document.getElementById('resetBtn');
 const colorChange = document.getElementById('chgClr');
 const blkWhite = document.getElementById('blkWhite');
 
-let colors=false;
+let colors = false;
 
-makeGrid(gridSize);
+function makeGrid(gridSize) {
 
-function makeGrid(gridSize){  
-   
     let newRow = makeRow(gridSize);
-    for(let j =0; j < gridSize; j++){
+    for (let j = 0; j < gridSize; j++) {
         let rowCopy = newRow.cloneNode(true);
         container.append(rowCopy);
     }
     let gridBox = document.getElementsByClassName('grid');
-    
+
     colorGrid(gridBox);
 }
 
-function makeRow(gridSize){
-    let boxHeight = (800/gridSize)-2;
-    let boxWidth = (800/gridSize)-2;
+function makeRow(gridSize) {
+    let boxHeight = (600 / gridSize) - 2;
+    let boxWidth = (600 / gridSize) - 2;
     let rows = document.createElement('div');
     rows.classList.add('rows');
 
-    for(let i = 0; i < gridSize; i++){
+    for (let i = 0; i < gridSize; i++) {
         let newDiv = document.createElement('div');
         newDiv.classList.add('grid');
-        newDiv.style.height = boxHeight+'px';
-        newDiv.style.width = boxWidth+'px';
+        newDiv.style.height = boxHeight + 'px';
+        newDiv.style.width = boxWidth + 'px';
         newDiv.style['background-color'] = 'rgba(0,0,0,0.0)';
         rows.append(newDiv);
     }
     return rows;
 }
 
-function resetGrid(){
-    let rows=document.getElementsByClassName('rows');
-   
-        while(container.firstChild){
-            container.removeChild(container.firstChild);
-        }
+function resetGrid() {
+
+    let rows = document.getElementsByClassName('rows');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 
     gridSize = prompt('What would you like the new grid to be?');
     makeGrid(gridSize);
 }
 
+function colorGrid(gridBox) {
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        if (!colors) {
+            gridBox[i].addEventListener('mouseover', () => {
+                changeOpacity(gridBox[i]);
+            });
+        }
+        else {
+            gridBox[i].addEventListener('mouseover', () => {
+                /* if it's a previously untouched box, assing new color to it*/
+                if (gridBox[i].style['background-color'] === 'rgba(0, 0, 0, 0)') {
+                    assignColor(gridBox[i]);
+                }
+                else {
+                    changeOpacity(gridBox[i]);
+                }
+            });
+        }
+    }
+}
+
+function assignColor(element) {
+
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    element.style['background-color'] = 'rgba(' + r + ',' + g + ',' + b + ',0.1)';
+
+    return element;
+}
+
+
+function changeOpacity(element) {
+
+    let opcValue = element.style['background-color'].match(/[^,]+(?=\))/)[0];
+    opcValue = Number(opcValue);
+
+    if (opcValue === 1)
+        return;
+    else {
+        console.log('it gets here');
+        opcValue += 0.1;
+        let newShade = element.style['background-color'].replace(/[^,]+(?=\))/, opcValue);
+        element.style['background-color'] = newShade;
+    }
+    return element;
+}
+
+makeGrid(gridSize);
 
 resetBtn.addEventListener('click', resetGrid);
 let gridBox = document.getElementsByClassName('grid');
 
-colorChange.addEventListener('click', () =>{
-    colors=true;
-    resetGrid();
-});
-blkWhite.addEventListener('click', () =>{
-    colors=false;
+colorChange.addEventListener('click', () => {
+    colors = true;
     resetGrid();
 });
 
-
-
-function colorGrid(gridBox){
-for(let i =0; i < gridSize*gridSize; i++){
-    if(!colors){
-        gridBox[i].addEventListener('mouseover', () =>{
-            /*matches last value of rgba, to get opacity*/
-            let opcValue = gridBox[i].style['background-color'].match(/[^,]+(?=\))/)[0];
-            opcValue=Number(opcValue);
-            if(opcValue===1)
-                return;
-            else{
-                opcValue+=0.1;
-                let newShade = gridBox[i].style['background-color'].replace(/[^,]+(?=\))/, opcValue);
-                gridBox[i].style['background-color'] = newShade;
-            }
-        });
-    }
-    else{
-        gridBox[i].addEventListener('mouseover', () =>{
-        if(gridBox[i].style['background-color'] === 'rgba(0, 0, 0, 0)'){
-            let r=Math.floor(Math.random()*256);
-            let g=Math.floor(Math.random()*256);
-            let b=Math.floor(Math.random()*256);
-          
-            gridBox[i].style['background-color'] = 'rgba('+r+','+g+','+b+',0.1)';    
-        }
-        
-        else{
-            let opcValue = gridBox[i].style['background-color'].match(/[^,]+(?=\))/)[0];
-            opcValue=Number(opcValue);
-
-            if(opcValue===1)
-                return;
-            else{
-                console.log('it gets here');
-                opcValue+=0.1;
-                let newShade = gridBox[i].style['background-color'].replace(/[^,]+(?=\))/, opcValue);
-                gridBox[i].style['background-color'] = newShade;
-            }
-         }
-    });
-    }
-   
-}
-}
-
+blkWhite.addEventListener('click', () => {
+    colors = false;
+    resetGrid();
+});
